0580e1efbadd5cf652950eb6c99e68596565bf64
the-bounty-hunter-rp <161922950+the-bounty-hunter-rp@users.noreply.github.com>
2025-01-20 16:54:52 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/rahul-754"
+                href="https://github.com/the_bounty-huntur-rp"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/rahul-kumar-93a220247/"
+                href="https://www.linkedin.com/in/rahul754/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
